f13a464a7355d8f79d25f8c09a4d7ed0c120d574
gonzalozilli-ai <gonzalo.zilli@2innovateit.com>
2026-03-13 17:10:29 -0300

    Update bloqueoConfigs.ps1

--- bloqueoConfigs.ps1
+++ bloqueoConfigs.ps1
@@ -1,11 +1,12 @@
+#bloquear power settings
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Force
 
 Set-ItemProperty `
  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
  -Name "SettingsPageVisibility" `
  -Value "hide:powersleep"
-
-
+# fin 
+#Bloquear panel de control
 New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Force
 
 Set-ItemProperty `
@@ -13,5 +14,5 @@ Set-ItemProperty `
  -Name "NoControlPanel" `
  -Value 1 `
  -Type DWord
-
+#fin
 gpupdate /force
