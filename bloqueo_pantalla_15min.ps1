1b433717ab94a05fce0f27b321b901dace37ed14
gonzalozilli-ai <gonzalo.zilli@2innovateit.com>
2026-03-13 16:47:06 -0300

    Update bloqueo_pantalla_15min.ps1

--- bloqueo_pantalla_15min.ps1
+++ bloqueo_pantalla_15min.ps1
@@ -1,6 +1,6 @@
 $loggedUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -Command `"`$guid = ((powercfg /getactivescheme) -split '\s+')[3]; powercfg /setacvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setdcvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setacvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setdcvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setactive `$guid`""
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -Command `"`$guid = ((powercfg /getactivescheme) -split '\s+')[3]; powercfg /setacvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setdcvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setacvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setdcvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setacvalueindex `$guid SUB_SLEEP HIBERNATEIDLE 900; powercfg /setdcvalueindex `$guid SUB_SLEEP HIBERNATEIDLE 900; powercfg /setactive `$guid`""
 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(10)
 
