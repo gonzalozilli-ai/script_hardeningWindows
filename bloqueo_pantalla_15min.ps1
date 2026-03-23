da793753f2259d1ec4e01bd45ecc1a3d2824f226
gonzalozilli-ai <gonzalo.zilli@2innovateit.com>
2026-03-23 13:32:31 -0300

    Update bloqueo_pantalla_15min.ps1

--- bloqueo_pantalla_15min.ps1
+++ bloqueo_pantalla_15min.ps1
@@ -1,10 +1,10 @@
-$loggedUser = (Get-WmiObject -Class Win32_ComputerSystem).UserName
+$loggedUser = "$env:USERDOMAIN\$env:USERNAME"
 
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -Command `"`$guid = ((powercfg /getactivescheme) -split '\s+')[3]; powercfg /setacvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setdcvalueindex `$guid SUB_VIDEO VIDEOIDLE 900; powercfg /setacvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setdcvalueindex `$guid SUB_SLEEP STANDBYIDLE 900; powercfg /setacvalueindex `$guid SUB_SLEEP HIBERNATEIDLE 900; powercfg /setdcvalueindex `$guid SUB_SLEEP HIBERNATEIDLE 900; powercfg /setactive `$guid`""
 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(10)
 
-$principal = New-ScheduledTaskPrincipal -UserId $loggedUser -LogonType Interactive -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId $loggedUser -LogonType InteractiveToken -RunLevel Highest
 
 Register-ScheduledTask -TaskName "AplicarBloqueo15min" -Action $action -Trigger $trigger -Principal $principal -Force
 
